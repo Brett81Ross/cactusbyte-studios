9362f0638e229b217a7062275b4e0e3904bb0515
Brett  Ross <rossbrett05@gmail.com>
2026-08-31 11:51:55 -0500

    Fix web manifest icons and rotation

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,11 +9,16 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#050807",
     theme_color: "#050807",
-    orientation: "portrait-primary",
     icons: [
       {
-        src: "/logo2.png",
-        sizes: "any",
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
