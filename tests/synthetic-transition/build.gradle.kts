5305b4121367d6aa00d02c59e96668ca3d6a6204
Brett  Ross <rossbrett05@gmail.com>
2026-09-02 09:51:34 -0500

    test: configure standalone Acelynn transition harness

---
+++ tests/synthetic-transition/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application") version "8.9.1"
+}
+
+android {
+    namespace = "com.cactusbyte.synthetictransition"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.cactusbyte.synthetictransition"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:rules:1.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
+}
