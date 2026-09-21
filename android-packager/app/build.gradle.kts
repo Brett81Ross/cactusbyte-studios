467f6a413351dbb85a7ac0f774ad7cc62ce5f107
Brett  Ross <rossbrett05@gmail.com>
2026-09-21 14:59:35 -0500

    release(acelynn): stage Play v1.2.0 RC1 identity

--- android-packager/app/build.gradle.kts
+++ android-packager/app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     signingConfigs {
