25d5999c973730491b996305482de93b0ebb5483
Brett  Ross <rossbrett05@gmail.com>
2026-09-21 20:22:18 -0500

    release: bump Acelynn Play candidate to 1.2.1

--- android-packager/app/build.gradle.kts
+++ android-packager/app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
     }
 
     signingConfigs {
